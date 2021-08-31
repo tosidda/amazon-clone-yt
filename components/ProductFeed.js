e3b46e1a209f2350927e4757ca5973d2124b7fa9
Sidda Chalamalasetty <tosidda@gmail.com>
2021-08-31 09:39:06 -0400

    updates

---
+++ components/ProductFeed.js
@@ -0,0 +1,38 @@
+import Product from "./Product"
+
+function ProductFeed({products}) {
+    return (
+    
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+            
+            {products.slice(0,4).map((product, p) => (
+                <Product key={p} product={product}/>
+            ))}
+
+            <img className="md:col-span-full" src="https://links.papareact.com/dyz/" alt="" />
+            <div className="md:col-span-2">
+            
+            {products.slice(4,5).map((product, p) => (
+                <Product key={p} product={product}/>
+            ))}
+
+           
+
+            
+        </div>
+        
+        {products.slice(5,products.length).map((product, p) => (
+                <Product key={p} product={product}/>
+            ))}
+       
+
+
+        
+
+
+        
+    </div>           
+    )
+}
+
+export default ProductFeed
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,19 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp')
+  ],
 }
